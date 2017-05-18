13a7f94963e8ce97870c5a2658c929304652b788
AgustinRow <agus.diaz.11@gmail.com>
2017-05-18 15:12:01 -0300

    Create p3_ej5.c
    Practica 3, ejercicio 5. SIn terminar funcion merge
    Merge: toma 2 arreglos y crea uno nuevo con la suma de cada uno de sus elementos.

---
+++ p3_ej5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include >stlib.h>
+/*¿como hago para corrobar la cantidad de elementos de uno sea la misma que el otro,
+para el caso que sean de distinto tamaño?*/
+
+int merge(int arr1[], int arr2[], cant_elementos){
+	
+}
+
+
+int main(){
+	int arr1[]= {10,2,3,5,12,65};
+	int arr2[]= {54,4,2,89,2,3};
+	int cant_elementos= 6;
+	int arr3[]=malloc(sizeof(arr1))
+	merge(arr1, arr2);
+	
+}
