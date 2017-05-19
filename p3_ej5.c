2bb77f4a2b2eb62ec428fdf62b96dc59d8aff189
AgustinRow <agus.diaz.11@gmail.com>
2017-05-18 22:59:43 -0300

    Update p3_ej5.c

--- p3_ej5.c
+++ p3_ej5.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
-#include >stlib.h>
+#include <stdlib.h>
 /*¿como hago para corrobar la cantidad de elementos de uno sea la misma que el otro,
 para el caso que sean de distinto tamaño?*/
 
-int merge(int arr1[], int arr2[], cant_elementos){
-	
+void suma(int arr1[], int arr2[],int arr3[], int cant_elementos){
+	int i;
+	for(i=0; i<cant_elementos; i++){
+		arr3[i]= arr1[i]+arr2[i];
+	}
 }
 
+void imprimir_arreglo(int array[], int cant){
+	int i;
+	for(i=0; i< cant; i++)	printf("%d\n", array [i]);
+}
 
 int main(){
 	int arr1[]= {10,2,3,5,12,65};
 	int arr2[]= {54,4,2,89,2,3};
 	int cant_elementos= 6;
-	int arr3[]=malloc(sizeof(arr1))
-	merge(arr1, arr2);
-	
+	int arr3[6];
+	suma(arr1, arr2, arr3, cant_elementos);
+	imprimir_arreglo(arr3, cant_elementos);	
+	return 0;
 }
